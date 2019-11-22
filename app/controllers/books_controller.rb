bffd9c463805120c08fefdf065a11006569aa02d
張維傑 <jie255057@gmail.com>
2019-11-22 16:58:18 +0800

    bookstore

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,21 @@
 class BooksController < ApplicationController
     
+  before_action :find_book, only:[:show, :edit, :update, :destroy]
+
+
     def index
       @book = Book.all
+    end
+
+    def show
+      # find_book
     end
 
     def new
       @book = Book.new
     end
-
-    def create
+		
+		def create
       
     @book = Book.new(book_params)
       
@@ -23,11 +30,11 @@ class BooksController < ApplicationController
     def edit
       # @book = Book.find_by(id: params[:id])
       # redirect_to root_path unless @book, notice: '查無此書'
-      @book = Book.find(params[:id])
+      # find_book
     end
 
     def update
-      @book = Book.find(params[:id])
+      # find_book
 
       if @book.update(book_params)
         redirect_to root_path, notice: '更新成功'
@@ -35,11 +42,25 @@ class BooksController < ApplicationController
         render :edit
       end
     end
+
+    def destroy
+      # find_book
+      @book.destroy
+      redirect_to root_path, notice: '資料已刪除'
+    end
     
 
 
-    # string parameters
+
     private
+
+
+    def find_book
+      @book = Book.find(params[:id])
+    end
+
+
+    # string parameters
     def book_params
         params.require(:book).permit(
                 :title,
