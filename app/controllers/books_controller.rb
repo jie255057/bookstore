aedc88cdae88bba27a9de62fa611c880a0d47543
張維傑 <jie255057@gmail.com>
2019-12-09 15:22:53 +0800

    add layout book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,12 @@
 class BooksController < ApplicationController
   before_action :find_book, only:[:show]
-
+  layout 'book'
     def index
       @books = Book.available
                   .with_attached_cover_image
                   .page(params[:page])
                   .per(3)
+      @publishers = Publisher.available
     end
 
     def show
