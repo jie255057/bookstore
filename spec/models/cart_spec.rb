97497c89bfccf76f14a5c81e5a7a15e7190a0998
張維傑 <jie255057@gmail.com>
2019-12-16 12:16:24 +0800

    add_cart_par2

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
-# 如果加了相同種類的商品到購物車裡，購買項目（CartItem）並不會增加，但商品的數量會改變。
 # 商品可以放到購物車裡，也可以再拿出來。
 # 每個 Cart Item 都可以計算它自己的金額（小計）。
 # 可以計算整台購物車的總消費金額。
@@ -12,6 +11,16 @@ RSpec.describe Cart, type: :model do
       cart = Cart.new
       cart.add_item(1)
       expect(cart.empty?).to be false
+    end
+
+    it "如果加了相同商品，購買項目不會增加，商品的數量會改變" do
+      cart = Cart.new
+
+      3.times { cart.add_item(1) }
+      2.times { cart.add_item(2) }
+
+      expect(cart.items.count).to be 2
+      expect(cart.items.first.quantity).to be 3
     end
   end
 end
