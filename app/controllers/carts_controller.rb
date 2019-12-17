a84ace7f550e620b3af2f31263ef0af4f876bdc3
張維傑 <jie255057@gmail.com>
2019-12-17 12:44:00 +0800

    add cart show

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,8 +1,17 @@
 class CartsController < ApplicationController
+  layout 'book'
   def add
     current_cart.add_item(params[:id])
     session['cart9527'] = current_cart.serialize
 
     redirect_to root_path, notice: 'ok'
+  end
+
+  def show
+  end
+
+  def destroy
+    session['cart9527'] = nil
+    redirect_to root_path, notice: '購物車已清空'
   end
 end
